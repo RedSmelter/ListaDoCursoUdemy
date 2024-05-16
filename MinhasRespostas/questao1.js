b0e9f81a9bf1f754db0624abe518aaeff8af7b1f
RedSmelter <eduardo2001fevereiro@gmail.com>
2024-05-16 15:32:59 -0300

    PRimeira questão feita

--- MinhasRespostas/questao1.js
+++ MinhasRespostas/questao1.js
@@ -1,3 +1,31 @@
-console.log('olá git')
-console.log('Salve  mano')
-console.log('staged changes')
+function manipulacoesMatematicas(a,b){
+
+    this.soma = function(a,b){
+        c = a + b
+        return c
+    }
+
+    this.subtracao = function(primeiroNumero,segundoNumero){
+        resultadoSubtracao = primeiroNumero - segundoNumero
+        return resultadoSubtracao
+    }
+
+}
+
+const notas = new manipulacoesMatematicas
+
+console.log(notas.soma(10,20))
+console.log(notas.subtracao(30,15))
+
+function manipuladosDiretamente(num1, num2){
+    soma = num1 + num2
+    subtracao = num1 - num2
+    multiplicacao = num1 * num2
+    divisao = num1/num2
+    
+    return console.log(`Soma: ${soma} Subtração: ${subtracao}
+    Multiplicação: ${multiplicacao} Divisão: ${divisao}`)
+
+}
+
+console.log(manipuladosDiretamente(10,20))
